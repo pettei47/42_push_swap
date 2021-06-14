b29e921b433ba94b29e66c55b337a1643680243a
pettei47 <kitagawa.teppei.33c@gmail.com>
2021-06-14 15:21:29 +0900

    split修正+双方向リストを定義

--- incs/push_swap.h
+++ incs/push_swap.h
@@ -6,10 +6,20 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 14:33:17 by teppei            #+#    #+#             */
-/*   Updated: 2021/06/14 15:10:36 by teppei           ###   ########.fr       */
+/*   Updated: 2021/06/14 15:19:59 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define  PUSH_SWAP_H
+# include <stdio.h>
+# include "../libft/libft.h"
+
+typedef	struct s_dlst
+{
+	long			value;
+	struct s_dlst	*next;
+	struct s_dlst	*prev;
+}					t_dlst;
+
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 14:32:10 by teppei            #+#    #+#             */
-/*   Updated: 2021/06/14 14:35:53 by teppei           ###   ########.fr       */
+/*   Updated: 2021/06/14 15:18:15 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	//int	*a;
+	//int	*b;
+	int	listsize;
+
+	listsize = argc - 1;
+	printf("listsize: %d\n", listsize);
 	(void)argv;
 	return (0);
 }
