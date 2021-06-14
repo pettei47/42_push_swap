5261a521f7d28ea059d112c5819b3ae2d055aeec
pettei47 <kitagawa.teppei.33c@gmail.com>
2021-06-14 16:29:14 +0900

    init dlstを作成

--- incs/push_swap.h
+++ incs/push_swap.h
@@ -6,14 +6,18 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 14:33:17 by teppei            #+#    #+#             */
-/*   Updated: 2021/06/14 15:19:59 by teppei           ###   ########.fr       */
+/*   Updated: 2021/06/14 16:08:46 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define  PUSH_SWAP_H
 # include <stdio.h>
+# include <limits.h>
+# include <stdbool.h>
 # include "../libft/libft.h"
+
+# define ARGLIMIT 10000
 
 typedef	struct s_dlst
 {
@@ -21,5 +25,26 @@ typedef	struct s_dlst
 	struct s_dlst	*next;
 	struct s_dlst	*prev;
 }					t_dlst;
+
+typedef	struct s_pair
+{
+	long			value;
+	long			id;
+}					t_pair;
+
+typedef	struct s_ps
+{
+	t_dlst			*ans;
+	t_pair			n[ARGLIMIT + 10];
+	long			def[ARGLIMIT + 10];
+	long			size;
+	long			awant;
+	long			bwant;
+	long			ans_result;
+	long			ans_turn;
+	char			ans_next[5][10];
+	bool			vflag;
+	bool			cflag;
+}					t_ps;
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,35 +6,78 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 14:32:10 by teppei            #+#    #+#             */
-/*   Updated: 2021/06/14 15:41:25 by teppei           ###   ########.fr       */
+/*   Updated: 2021/06/14 16:28:41 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/push_swap.h"
 
-int	ps_puterror(char *s, char **p, int ret)
+void	ps_dlst_clear(t_dlst *a)
 {
-	if (s)
-		free(s);
-	if (p)
-		ft_free_ptr(p);
+	t_dlst	*tmp;
+
+	a->prev->next = NULL;
+	while (a)
+	{
+		tmp = a->next;
+		a->prev = NULL;
+		free(a);
+		a = tmp;
+	}
+}
+
+void	ps_free_ps(t_dlst *a, t_dlst *b, t_ps *ps)
+{
+	if (a)
+		ps_dlst_clear(a);
+	if (b)
+		ps_dlst_clear(b);
+	if (ps)
+	{
+		ps_dlst_clear(ps->ans);
+		free(ps);
+	}
+}
+
+int	ps_puterror(t_dlst *a, t_dlst *b, t_ps *ps, int ret)
+{
+	ps_free_ps(a, b, ps);
 	ft_putstr_fd("Error\n", 2);
 	return (ret);
 }
 
+t_dlst	*ps_init_dlst(void)
+{
+	t_dlst	*dl;
+
+	dl = (t_dlst *)malloc(sizeof(t_dlst));
+	if (!dl)
+		exit(ps_puterror(NULL, NULL, NULL, 1));
+	dl->value = -1;
+	dl->next = dl;
+	dl->prev = dl;
+	return (dl);
+}
+
+t_ps	*ps_init_ps(void)
+{
+	return (NULL);
+}
+
 int	main(int argc, char **argv)
 {
-	//int	*a;
-	//int	*b;
-	int	listsize;
+	int		listsize;
+	t_ps	*ps;
+	t_dlst	*a;
+	t_dlst	*b;
 
 	listsize = argc - 1;
-	if (listsize < 1)
+	if (listsize < 1 || ARGLIMIT < listsize)
 		return (1);
-	ft_putstrs_fd(argv, '\n', 1);
 	if (ft_ptrdigit(++argv) == 0)
-		return (ps_puterror(NULL, NULL, 1));
-	printf("listsize: %d\n", listsize);
-	(void)argv;
+		return (ps_puterror(NULL, NULL, NULL, 1));
+	ps = ps_init_ps();
+	a = ps_init_dlst();
+	b = ps_init_dlst();
 	return (0);
 }
